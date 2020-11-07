04e53f54b720e63376e8d3b417d0bcfb0166daaf
glenn <glenn.ludszuweit@gmail.com>
2020-11-07 23:14:29 +0100

    sample react portfolio

---
+++ src/Components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className='view'>
+      <h1 className='view-title'>Contact Me</h1>
+      <div className='view-description'>
+        <h2>My Address</h2>
+        <h2>My Telephone</h2>
+        <h2>My Email</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
